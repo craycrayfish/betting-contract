753eef9eaa252f82284aa52e185a52acdb9ca975
craycrayfish <shawntan72@gmail.com>
2022-05-06 00:24:34 +0100

    tests for contract deployment, outcome adding, bet placement

---
+++ tests/conftest.py
@@ -0,0 +1,16 @@
+from brownie import network
+from scripts.deploy import deploy_bookie
+import pytest
+
+from scripts.utils import LOCAL_BLOCKCHAIN_ENVIRONMENTS
+
+
+@pytest.fixture()
+def check_local_blockchain_envs():
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip()
+
+
+@pytest.fixture()
+def bookie_contract():
+    return deploy_bookie()
---
+++ tests/test_bookie_unit.py
@@ -0,0 +1,16 @@
+from scripts.utils import (
+    get_account,
+)
+from brownie import exceptions
+from web3 import Web3
+import pytest
+
+
+def test_create_outcome_and_place_bet(check_local_blockchain_envs, bookie_contract):
+    account = get_account()
+    bet_outcome = 1
+    bet_value = 2
+    bookie_contract.start_betting({"from": account})
+    bookie_contract.add_outcome({"from": account})
+    bookie_contract.place_bet(bet_outcome, {"from": account, "value": bet_value})
+    assert bookie_contract.get_bets(account) == ((bet_outcome,), (bet_value,))
