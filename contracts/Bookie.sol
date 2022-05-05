9f5c2ef628631e1974a202e35e242aa0a4b87a17
craycrayfish <shawntan72@gmail.com>
2022-05-05 22:31:35 +0100

    added vars and place_bet logic

--- contracts/Bookie.sol
+++ contracts/Bookie.sol
@@ -5,34 +5,50 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Bookie is Ownable {
     // Contract shall handle the betting for a single binary outcome
-    address payable[] public players;
+    uint256 public no_bets;
+    mapping(address => uint256[]) public bets; // bettor positions in bet arrays
     uint256[] public bet_outcomes;
     uint256[] public bet_amounts;
-    uint256[] public outcome_pool;
     uint256 public no_outcomes;
+    mapping(uint256 => uint256) public payouts;
 
     enum STATE {
-        BETTING_CLOSED,
+        CLOSED,
         PRE_GAME,
         MID_GAME,
         POST_GAME
     }
+    STATE public state;
 
     constructor() public {
         // Initialises variables
+        state = STATE.CLOSED;
+        no_bets = 0;
+    }
+
+    function start_betting() public onlyOwner {
+        require(state == STATE.CLOSED, "Betting already started!");
+        state = STATE.PRE_GAME;
     }
 
     function get_game_state() public {
         // Call chainlink node to get live results
     }
 
-    function calculate_odds() public {
+    function update_odds() public {
         // Calculates the live payouts based on bets placed on each outcome
     }
 
     function place_bet(uint256 _outcome) public payable {
         // Saves the bets made by the player
+        require(state != STATE.CLOSED, "Betting not allowed now");
         require(_outcome < no_outcomes, "Selected outcome not valid");
+        require(msg.value > 0, "Bet value must be greater than 0");
+
+        bets[msg.sender].push(no_bets);
+        bet_outcomes.push(_outcome);
+        bet_amounts.push(msg.value);
+        update_odds();
     }
 
     function calculate_payout(address _address) private returns (uint256) {
